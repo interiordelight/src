61586b3a5b807bae463dab2180f0f748520660a4
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2017-12-13 14:34:51 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,7 @@ yarn build-projects-json
 next build
 next export
 yarn build-seo-files
-cp out-files/manifest.json out
-cp out-files/CNAME out
-cp out-files/.nojekyll out
+cp out-files/* out
 cp readme.md out
 cd out
 git add -A
