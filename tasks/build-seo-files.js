92586a833000f2bf3963a6f046f04fbe0af0a2b3
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2019-02-13 15:49:55 +0200

    Update deps & fix an error in seo files generation

--- tasks/build-seo-files.js
+++ tasks/build-seo-files.js
@@ -9,7 +9,9 @@ writeFileSync(`${dstPath}/robots.txt`, `User-agent: *\n${domainUrl}/sitemap.xml`
 const sitemapParams = {
   root: domainUrl,
   now: new Date().toJSON().slice(0, 10),
-  projects: readdirSync(`${__dirname}/../projects-json`).map(fileName => fileName.slice(0, -5))
+  projects: readdirSync(`${__dirname}/../projects-json`)
+    .map(fileName => fileName.slice(0, -5))
+    .filter(fileName => fileName !== 'index')
 };
 
 writeFileSync(`${dstPath}/sitemap.xml`, pug.renderFile(`${__dirname}/sitemap.pug`, sitemapParams));
