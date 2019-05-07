5c745e486800b7b486ab1d79b98659ac4321d963
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2019-05-07 19:32:36 +0300

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,12 @@ yarn build-projects-json
 next build
 next export
 yarn build-seo-files
-cp out-files/* out
+cp -R out-files/. out
 cp readme.md out
 cd out
+git init
+git remote add origin git@github.com:interiordelight/interiordelight.github.io.git
 git add -A
 git commit -m 'Auto publish commit'
-git push --force
+git push --force --set-upstream origin master
 cd ..
