ac89868b44b7bb731237a74a7c73565d3ae21d45
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2019-10-27 17:33:05 +0200

    Fix icons in production

--- components/Layout.js
+++ components/Layout.js
@@ -145,52 +145,52 @@ const Layout = ({ title, activeMenuItem, showFooterBanners, children }) => (
       }
 
       .icon-home:before {
-        content: '\\e900';
+        content: '\e900';
       }
       .icon-info:before {
-        content: '\\e901';
+        content: '\e901';
       }
       .icon-image:before {
-        content: '\\e902';
+        content: '\e902';
       }
       .icon-talk:before {
-        content: '\\e903';
+        content: '\e903';
       }
       .icon-left:before {
-        content: '\\e904';
+        content: '\e904';
       }
       .icon-right:before {
-        content: '\\e905';
+        content: '\e905';
       }
       .icon-close:before {
-        content: '\\e906';
+        content: '\e906';
       }
       .icon-facebook:before {
-        content: '\\e907';
+        content: '\e907';
       }
       .icon-twitter:before {
-        content: '\\e908';
+        content: '\e908';
       }
       .icon-google:before {
-        content: '\\e909';
+        content: '\e909';
       }
       .icon-behance:before {
-        content: '\\e90a';
+        content: '\e90a';
       }
       .icon-pinterest:before {
-        content: '\\e90b';
+        content: '\e90b';
       }
       .icon-linkedin:before {
-        content: '\\e90c';
+        content: '\e90c';
       }
       .icon-email:before {
-        content: '\\e90d';
+        content: '\e90d';
       }
       .icon-skype:before {
-        content: '\\e90e';
+        content: '\e90e';
       }
       .icon-github:before {
-        content: '\\e90f';
+        content: '\e90f';
       }
     `}</style>
   </div>
