c5794d3ec5ce88e4bc6dae53c1ac3ca5bb1bfe2a
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2022-01-03 23:29:25 +0200

    Remove old SEO meta tags

--- components/PortfolioHead.js
+++ components/PortfolioHead.js
@@ -1,17 +1,5 @@
 import Head from 'next/head';
-
-import { domain, domainUrl } from '../config';
-import projects from '../projects-json/index.json';
-
-const twitterImageIndexes = [1, 2, 3, 4];
-
-const renderTwitterImage = index => (
-  <meta
-    key={index}
-    property={`twitter:image${index}`}
-    content={`${domainUrl}/static/pics/o/${projects[index].slug}-cover.jpg`}
-  />
-);
+import { domain } from '../config';
 
 export default () => (
   <Head>
@@ -24,16 +12,6 @@ export default () => (
       name="keywords"
       content="interior design, 3d, modeling, rendering, post processing, rhino, vray, maxwell, photoshop, portfolio"
     />
-    <meta name="twitter:card" content="gallery" />
-    <meta name="twitter:creator" content="iriflorescu" />
-    <meta name="twitter:domain" content={`www.${domain}`} />
-    <meta name="twitter:title" content="InteriorDelight.RO Portfolio" />
-    <meta
-      name="twitter:description"
-      // eslint-disable-next-line max-len
-      content="See my portfolio of professional interior design, decoration, visualization, 3D modeling, rendering and post-processing."
-    />
-    {twitterImageIndexes.map(renderTwitterImage)}
     <meta property="og:site_name" content={domain} />
     <meta property="og:title" content="Portfolio" />
     <meta
@@ -42,7 +20,5 @@ export default () => (
       content="See my portfolio of professional interior design, decoration, visualization, 3D modeling, rendering and post-processing"
     />
     <meta property="og:type" content="article" />
-    <meta property="og:author" content="100003892666191" />
-    <meta property="og:publisher" content="215465541907600" />
   </Head>
 );
--- components/ProjectHead.js
+++ components/ProjectHead.js
@@ -1,9 +1,8 @@
-import PropTypes from 'prop-types';
 import Head from 'next/head';
+import PropTypes from 'prop-types';
+import { domainUrl } from '../config';
 
-import { domain, domainUrl } from '../config';
-
-const renderOgSeeAlsoProject = project => (
+const renderOgSeeAlsoProject = (project) => (
   <meta key={project.index} property="og:see_also" content={`${domainUrl}/portfolio/${project.slug}/`} />
 );
 
@@ -16,18 +15,10 @@ const ProjectHead = ({ slug, title, description, otherProjects }) => {
         name="keywords"
         content="interior design, 3d, modeling, rendering, post processing, rhino, vray, maxwell, photoshop, portfolio"
       />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:creator" content="iriflorescu" />
-      <meta name="twitter:domain" content={domain} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={absoluteCoverUrl} />
       <meta property="og:title" content={title} />
       <meta property="og:image" content={absoluteCoverUrl} />
       <meta property="og:description" content={description} />
       <meta property="og:type" content="article" />
-      <meta property="og:author" content="100003892666191" />
-      <meta property="og:publisher" content="215465541907600" />
       {otherProjects.map(renderOgSeeAlsoProject)}
     </Head>
   );
@@ -40,9 +31,9 @@ ProjectHead.propTypes = {
   otherProjects: PropTypes.arrayOf(
     PropTypes.shape({
       index: PropTypes.number,
-      slug: PropTypes.string
+      slug: PropTypes.string,
     })
-  ).isRequired
+  ).isRequired,
 };
 
 export default ProjectHead;
